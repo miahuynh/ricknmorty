05e11e392975240508ab33b0a39b205762bab0fa
Mia <ngocmhuynh5@gmail.com>
2018-07-03 12:38:50 -0700

    completed boiler plate for solo project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+    return <div>REACT </div>;
+};
+
+
+ReactDOM.render(<Index/>, document.getElementById("index"));
